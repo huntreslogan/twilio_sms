84ae0706fd1c0172c5680d8ff4b459b05b0981ac
Jessica Hunt <huntres@me.com>
2015-10-01 14:17:40 -0700

    Message tracking

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import twilio.twiml
 
 app = Flask(__name__)
@@ -10,16 +10,23 @@ callers = {
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
 
-	from_number = request.values.get('From', None)
-	if from_number in callers:
-		message = callers[from_number] + ", thanks for the message!"
-	else:
-		message = "Monkey, thanks for the message!"
+	counter = session.get('counter', 0)
 
+	counter += 1
+
+	session['counter'] = counter
+
+	from_number = request.values.get('From')
+	if from_number in callers:
+		name = callers[from_number]
+	else:
+		name = "Monkey"
+
+	message = "".join([name, " has messaged ", request.values.get('To'), " ",
+		str(counter), " times."])
 	resp = twilio.twiml.Response()
-	resp.message(message)
-	# with resp.message("Hello, Mobile Monkey") as m:
-	# 	m.media("https://s-media-cache-ak0.pinimg.com/236x/18/65/05/186505928c838149777b36205bc25684.jpg")
+	resp.sms(message)
+
 	return str(resp)
 
 if __name__ == "__main__":
